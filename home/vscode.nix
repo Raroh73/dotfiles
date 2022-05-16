a6f5328a804ab43d7caf8989c12afcd2fed37b2e
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2022-05-16 16:41:50 +0200

    feat: add more vscode settings

--- home/vscode.nix
+++ home/vscode.nix
@@ -9,10 +9,13 @@
     ];
     userSettings = {
       "editor.formatOnSave" = true;
+      "editor.rulers" = [ 80 120 ];
+      "extensions.ignoreRecommendations" = true;
       "files.eol" = "\n";
       "files.insertFinalNewline" = true;
       "files.trimFinalNewlines" = true;
       "files.trimTrailingWhitespace" = true;
+      "telemetry.telemetryLevel" = "off";
     };
   };
 }
