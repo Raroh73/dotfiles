5540e686bb420fa29a648e2e53a750d402895e66
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2022-05-19 14:13:24 +0200

    refactor: use with in vscode.nix

--- home/vscode.nix
+++ home/vscode.nix
@@ -1,11 +1,11 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    extensions = [
-      pkgs.vscode-extensions.esbenp.prettier-vscode
-      pkgs.vscode-extensions.jnoortheen.nix-ide
-      pkgs.vscode-extensions.matklad.rust-analyzer
-      pkgs.vscode-extensions.tamasfe.even-better-toml
+    extensions = with pkgs.vscode-extensions; [
+      esbenp.prettier-vscode
+      jnoortheen.nix-ide
+      matklad.rust-analyzer
+      tamasfe.even-better-toml
     ];
     userSettings = {
       "editor.fontFamily" = "Cascadia Code";
