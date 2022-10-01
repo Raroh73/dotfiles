a42a395baf4b6f30df12be3003e74136bdec40ff
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2022-10-01 20:28:06 +0200

    feat: add automatic garbage collecting

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -8,6 +8,11 @@
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 
   age.secrets = {
