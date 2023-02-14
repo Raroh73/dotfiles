6c1dad2fd6052745e1018563f5168a116099a524
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-02-14 14:42:27 +0100

    feat: remove unnecessary nushell setting

--- hosts/earth/home/nushell.nix
+++ hosts/earth/home/nushell.nix
@@ -1,18 +1,5 @@
 { ... }: {
   programs.nushell = {
     enable = true;
-    configFile.text =  ''
-      let-env config = {
-        hooks: {
-          pre_prompt: [{
-            code: "
-              let direnv = (direnv export json | from json)
-              let direnv = if ($direnv | length) == 1 { $direnv } else { {} }
-              $direnv | load-env
-            "
-          }]
-        }
-      }
-    '';
   };
 }
