76f54e6bea30c692c80a92f7cec4b8904adda462
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-03-09 11:41:59 +0100

    feat: use https only mode

--- hosts/earth/home/firefox.nix
+++ hosts/earth/home/firefox.nix
@@ -5,6 +5,10 @@
       "Raroh73" = {
         extensions = with pkgs.nur.repos.rycee.firefox-addons;
           [ ublock-origin ];
+        settings = {
+          "dom.security.https_only_mode" = true;
+          "dom.security.https_only_mode_ever_enabled" = true;
+        };
       };
     };
   };
