3212f3762f9eff0e5fb9b5136d4deb7ac587e7e6
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-03-09 15:55:35 +0100

    feat: disable trimming URLs

--- hosts/earth/home/firefox.nix
+++ hosts/earth/home/firefox.nix
@@ -7,6 +7,7 @@
           [ ublock-origin ];
         settings = {
           "browser.aboutConfig.showWarning" = false;
+          "browser.urlbar.trimURLs" = false;
           "dom.security.https_only_mode" = true;
           "dom.security.https_only_mode_ever_enabled" = true;
           "extensions.pocket.enabled" = false;
