80a74077759bd4518536704b4b412dd4e08945cb
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-03-10 11:34:57 +0100

    refactor: use extraConfig for signing

--- hosts/earth/home/git.nix
+++ hosts/earth/home/git.nix
@@ -3,19 +3,24 @@
     enable = true;
     userName = "Raroh73";
     userEmail = "96078496+Raroh73@users.noreply.github.com";
-    signing = {
-      key = "7F60D3C92F885B70";
-      signByDefault = true;
-    };
     extraConfig = {
       apply = {
         whitespace = "fix";
+      };
+      commit = {
+        gpgSign = true;
       };
       core = {
         whitespace = "trailing-space,space-before-tab";
       };
       pull = {
         ff = "only";
+      };
+      tag = {
+        gpgSign = true;
+      };
+      user = {
+        signingKey = "7F60D3C92F885B70";
       };
     };
   };
