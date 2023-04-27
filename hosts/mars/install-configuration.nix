9eac3cd06be5574dbd474f104f0b0ad83e003946
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-04-27 20:46:21 +0200

    feat: add mars installation config

---
+++ hosts/mars/install-configuration.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, ... }: {
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXOS_SD";
+      fsType = "ext4";
+    };
+  };
+
+  boot.loader.raspberryPi = {
+    enable = true;
+    version = 3;
+    uboot.enable = true;
+  };
+
+  networking = {
+    firewall.enable = true;
+    hostName = "mars";
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "Europe/Warsaw";
+
+  users.mutableUsers = false;
+
+  users.users.root.hashedPassword = "!";
+
+  users.users.raroh73 = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    hashedPassword = "$y$j9T$E7B.wD/rB2Z6LnU05C86N1$tREsxNWhOg2Zul8lRrSaUvNX3icaHcV1Ac3QfMRAFXA";
+    packages = with pkgs; [
+      git
+    ];
+  };
+
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  system.stateVersion = "22.11";
+}
