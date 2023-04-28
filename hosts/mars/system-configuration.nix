978a3a1096610a5ddf439498c397c63546b23ea5
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-04-28 20:29:48 +0200

    refactor: move git to home config

--- hosts/mars/system-configuration.nix
+++ hosts/mars/system-configuration.nix
@@ -65,7 +65,6 @@
         extraGroups = [ "networkmanager" "wheel" ];
         passwordFile = config.age.secrets.raroh73-mars-password.path;
         openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMQi95k0yKYkgnH8r3COiPPyBNqi6pdxyHnGl3qgsshP raroh73@mars" ];
-        packages = with pkgs; [ git ];
         shell = pkgs.nushell;
       };
       root.hashedPassword = "!";
