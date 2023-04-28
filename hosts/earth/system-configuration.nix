311b2a612489e4dcad59b0ec975c7d70e2ed8f9c
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-04-28 14:41:30 +0200

    style: reformat networking

--- hosts/earth/system-configuration.nix
+++ hosts/earth/system-configuration.nix
@@ -24,11 +24,13 @@
   };
 
   networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [ 8008 ];
+    };
     hostName = "earth";
     nameservers = [ "193.110.81.0#dns0.eu" "2a0f:fc80::#dns0.eu" "185.253.5.0#dns0.eu" "2a0f:fc81::#dns0.eu" ];
-    networkmanager = {
-      enable = true;
-    };
+    networkmanager.enable = true;
   };
 
   services.resolved = {
@@ -97,11 +99,6 @@
   };
 
   virtualisation.podman.enable = true;
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ 8008 ];
-  };
 
   programs.steam.enable = true;
 
