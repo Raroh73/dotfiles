82e5d54fb26c46e79c6a72d73f7e54950124a804
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-05-02 17:18:37 +0200

    fix: fix tls

--- hosts/mars/system-configuration.nix
+++ hosts/mars/system-configuration.nix
@@ -113,9 +113,11 @@
         serverAliases = [ "www.raroh73.xyz" ];
         useACMEHost = "raroh73.xyz";
         extraConfig = ''
-          client_auth {
-            mode require_and_verify
-            trusted_ca_cert_file /etc/ssl/certs/authenticated_origin_pull_ca.pem
+          tls {
+            client_auth {
+              mode require_and_verify
+              trusted_ca_cert_file /etc/ssl/certs/authenticated_origin_pull_ca.pem
+            }
           }
           root * /srv/www/raroh73_xyz
           file_server
