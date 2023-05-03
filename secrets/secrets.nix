77fbcdcd40540326b06ed3469abc77fb24a4d9e2
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-05-03 18:49:44 +0200

    chore: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   agenix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBerp1nA9AU2MUHk8/Fq+OvX0L2PiMqgQh12Zdm0cMyK agenix";
   earth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOt72SaSNMlIcwwnmjAp3+oSAc9avEAkFyLdbS9xoShy root@earth";
-  mars = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINkLR7LepjahrdQxYkiyvnY9tYG4aI++sec1+wiNFBUJ root@mars";
+  mars = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJCqtsxDNBWMQKu3xxU8Pv87HdYnM5CUCkECqLBC7tdQ root@mars";
 in
 {
   "cloudflare-token.age".publicKeys = [ agenix mars ];
