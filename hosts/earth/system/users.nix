ba30f98b20922093e001c32b9b9dd551a255af25
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-05-09 14:08:07 +0200

    feat: add authorized key to earth

--- hosts/earth/system/users.nix
+++ hosts/earth/system/users.nix
@@ -7,6 +7,7 @@
         description = "Raroh73";
         extraGroups = [ "networkmanager" "podman" "wheel" ];
         passwordFile = config.age.secrets.raroh73-earth-password.path;
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGK2oF5Zor8L0zXEldXvbHfLYe1XDHOATS6v2yX5PCHK raroh73@earth" ];
         shell = pkgs.nushell;
       };
       root.hashedPassword = "!";
