770134cf593bfd96c7e6cedd271b8104050ab735
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-05-11 18:09:13 +0200

    chore: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   agenix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBerp1nA9AU2MUHk8/Fq+OvX0L2PiMqgQh12Zdm0cMyK agenix";
   earth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOt72SaSNMlIcwwnmjAp3+oSAc9avEAkFyLdbS9xoShy root@earth";
-  mars = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINjpc+NN158vjAuxupE/VAcoI5t/Fino7UO3fUISUBwR root@mars";
+  mars = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJC4hkSZTfBsso4IE/r4qXAB0crFSG5mLVZ96jALinn8 root@mars";
 in
 {
   "backup-mars-environment.age".publicKeys = [ agenix mars ];
