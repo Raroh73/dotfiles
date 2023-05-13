07bf6003a9220b77256e9410e7b64ecaa7ff62b9
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-05-13 21:00:20 +0200

    feat: add authelia

--- hosts/mars/system-configuration.nix
+++ hosts/mars/system-configuration.nix
@@ -49,12 +49,52 @@
   };
 
   age.secrets = {
+    authelia-main-jwt = {
+      file = ../../secrets/authelia-main-jwt.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-oidc-hmac = {
+      file = ../../secrets/authelia-main-oidc-hmac.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-oidc-key = {
+      file = ../../secrets/authelia-main-oidc-key.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-settings = {
+      file = ../../secrets/authelia-main-settings.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-storage = {
+      file = ../../secrets/authelia-main-storage.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-users = {
+      file = ../../secrets/authelia-main-users.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
     backup-mars-environment.file = ../../secrets/backup-mars-environment.age;
     backup-mars-password.file = ../../secrets/backup-mars-password.age;
     backup-mars-repository.file = ../../secrets/backup-mars-repository.age;
     cloudflare-token.file = ../../secrets/cloudflare-token.age;
     lego-token.file = ../../secrets/lego-token.age;
     miniflux-admin-credentials.file = ../../secrets/miniflux-admin-credentials.age;
+    miniflux-oauth2-id = {
+      file = ../../secrets/miniflux-oauth2-id.age;
+      owner = "miniflux";
+      group = "miniflux";
+    };
+    miniflux-oauth2-secret = {
+      file = ../../secrets/miniflux-oauth2-secret.age;
+      owner = "miniflux";
+      group = "miniflux";
+    };
     raroh73-mars-password.file = ../../secrets/raroh73-mars-password.age;
   };
 
@@ -85,7 +125,7 @@
   services.cloudflare-dyndns = {
     enable = true;
     apiTokenFile = config.age.secrets.cloudflare-token.path;
-    domains = [ "raroh73.xyz" "www.raroh73.xyz" "miniflux.raroh73.xyz" "webhook.raroh73.xyz" ];
+    domains = [ "raroh73.xyz" "www.raroh73.xyz" "miniflux.raroh73.xyz" "webhook.raroh73.xyz" "authelia.raroh73.xyz" ];
     proxied = true;
   };
 
@@ -104,6 +144,11 @@
         email = "me@raroh73.xyz";
       };
       "webhook.raroh73.xyz" = {
+        credentialsFile = config.age.secrets.lego-token.path;
+        dnsProvider = "cloudflare";
+        email = "me@raroh73.xyz";
+      };
+      "authelia.raroh73.xyz" = {
         credentialsFile = config.age.secrets.lego-token.path;
         dnsProvider = "cloudflare";
         email = "me@raroh73.xyz";
@@ -148,12 +193,26 @@
           reverse_proxy localhost:9000
         '';
       };
+      "authelia.raroh73.xyz" = {
+        useACMEHost = "authelia.raroh73.xyz";
+        extraConfig = ''
+          reverse_proxy localhost:9091
+        '';
+      };
     };
   };
 
   services.miniflux = {
     enable = true;
     adminCredentialsFile = config.age.secrets.miniflux-admin-credentials.path;
+    config = {
+      OAUTH2_PROVIDER = "oidc";
+      OAUTH2_CLIENT_ID_FILE = config.age.secrets.miniflux-oauth2-id.path;
+      OAUTH2_CLIENT_SECRET_FILE = config.age.secrets.miniflux-oauth2-secret.path;
+      OAUTH2_REDIRECT_URL = https://miniflux.raroh73.xyz/oauth2/oidc/callback;
+      OAUTH2_OIDC_DISCOVERY_ENDPOINT = "https://authelia.raroh73.xyz";
+      OAUTH2_USER_CREATION = "1";
+    };
   };
 
   services.restic.backups = {
@@ -184,6 +243,17 @@
 
   environment.shells = with pkgs; [ nushell ];
   environment.systemPackages = with pkgs; [ libraspberrypi ];
+
+  services.authelia.instances.main = {
+    enable = true;
+    secrets = {
+      jwtSecretFile = config.age.secrets.authelia-main-jwt.path;
+      oidcHmacSecretFile = config.age.secrets.authelia-main-oidc-hmac.path;
+      oidcIssuerPrivateKeyFile = config.age.secrets.authelia-main-oidc-key.path;
+      storageEncryptionKeyFile = config.age.secrets.authelia-main-storage.path;
+    };
+    settingsFiles = [ config.age.secrets.authelia-main-settings.path ];
+  };
 
   system.stateVersion = "22.11";
 }
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,12 +4,20 @@ let
   mars = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJC4hkSZTfBsso4IE/r4qXAB0crFSG5mLVZ96jALinn8 root@mars";
 in
 {
+  "authelia-main-jwt.age".publicKeys = [ agenix mars ];
+  "authelia-main-oidc-hmac.age".publicKeys = [ agenix mars ];
+  "authelia-main-oidc-key.age".publicKeys = [ agenix mars ];
+  "authelia-main-settings.age".publicKeys = [ agenix mars ];
+  "authelia-main-storage.age".publicKeys = [ agenix mars ];
+  "authelia-main-users.age".publicKeys = [ agenix mars ];
   "backup-mars-environment.age".publicKeys = [ agenix mars ];
   "backup-mars-password.age".publicKeys = [ agenix mars ];
   "backup-mars-repository.age".publicKeys = [ agenix mars ];
   "cloudflare-token.age".publicKeys = [ agenix mars ];
   "lego-token.age".publicKeys = [ agenix mars ];
   "miniflux-admin-credentials.age".publicKeys = [ agenix mars ];
+  "miniflux-oauth2-id.age".publicKeys = [ agenix mars ];
+  "miniflux-oauth2-secret.age".publicKeys = [ agenix mars ];
   "raroh73-earth-password.age".publicKeys = [ agenix earth ];
   "raroh73-mars-password.age".publicKeys = [ agenix mars ];
 }
