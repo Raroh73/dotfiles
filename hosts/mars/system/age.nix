09cbeab4a0477ce3a54c3b2ea2e0097742aaae98
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-05-14 20:46:06 +0200

    refactor: move age to separate module

---
+++ hosts/mars/system/age.nix
@@ -0,0 +1,41 @@
+_: {
+  age.secrets = {
+    authelia-main-jwt = {
+      file = ../../../secrets/authelia-main-jwt.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-oidc-hmac = {
+      file = ../../../secrets/authelia-main-oidc-hmac.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-oidc-key = {
+      file = ../../../secrets/authelia-main-oidc-key.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-settings = {
+      file = ../../../secrets/authelia-main-settings.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-storage = {
+      file = ../../../secrets/authelia-main-storage.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    authelia-main-users = {
+      file = ../../../secrets/authelia-main-users.age;
+      owner = "authelia-main";
+      group = "authelia-main";
+    };
+    backup-mars-environment.file = ../../../secrets/backup-mars-environment.age;
+    backup-mars-password.file = ../../../secrets/backup-mars-password.age;
+    backup-mars-repository.file = ../../../secrets/backup-mars-repository.age;
+    cloudflare-token.file = ../../../secrets/cloudflare-token.age;
+    lego-token.file = ../../../secrets/lego-token.age;
+    miniflux-admin-credentials.file = ../../../secrets/miniflux-admin-credentials.age;
+    raroh73-mars-password.file = ../../../secrets/raroh73-mars-password.age;
+  };
+}
