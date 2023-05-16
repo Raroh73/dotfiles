121be982e60aa9d497dbcdefe2e1fee1d7b058fd
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-05-16 15:35:45 +0200

    refactor: move main domain up

--- hosts/mars/system/acme.nix
+++ hosts/mars/system/acme.nix
@@ -2,12 +2,12 @@
   security.acme = {
     acceptTerms = true;
     certs = {
-      "authelia.raroh73.xyz" = { };
-      "lldap.raroh73.xyz" = { };
-      "miniflux.raroh73.xyz" = { };
       "raroh73.xyz" = {
         extraDomainNames = [ "www.raroh73.xyz" ];
       };
+      "authelia.raroh73.xyz" = { };
+      "lldap.raroh73.xyz" = { };
+      "miniflux.raroh73.xyz" = { };
       "shiori.raroh73.xyz" = { };
       "webhook.raroh73.xyz" = { };
     };
