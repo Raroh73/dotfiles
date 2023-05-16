5b4fddde77d6a7cba21bceff51664e164041b2e4
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-05-16 15:33:30 +0200

    refactor: rewrite acme module

--- hosts/mars/system/acme.nix
+++ hosts/mars/system/acme.nix
@@ -2,37 +2,19 @@
   security.acme = {
     acceptTerms = true;
     certs = {
+      "authelia.raroh73.xyz" = { };
+      "lldap.raroh73.xyz" = { };
+      "miniflux.raroh73.xyz" = { };
       "raroh73.xyz" = {
-        credentialsFile = config.age.secrets.lego-token.path;
-        dnsProvider = "cloudflare";
-        email = "me@raroh73.xyz";
         extraDomainNames = [ "www.raroh73.xyz" ];
       };
-      "miniflux.raroh73.xyz" = {
-        credentialsFile = config.age.secrets.lego-token.path;
-        dnsProvider = "cloudflare";
-        email = "me@raroh73.xyz";
-      };
-      "webhook.raroh73.xyz" = {
-        credentialsFile = config.age.secrets.lego-token.path;
-        dnsProvider = "cloudflare";
-        email = "me@raroh73.xyz";
-      };
-      "authelia.raroh73.xyz" = {
-        credentialsFile = config.age.secrets.lego-token.path;
-        dnsProvider = "cloudflare";
-        email = "me@raroh73.xyz";
-      };
-      "shiori.raroh73.xyz" = {
-        credentialsFile = config.age.secrets.lego-token.path;
-        dnsProvider = "cloudflare";
-        email = "me@raroh73.xyz";
-      };
-      "lldap.raroh73.xyz" = {
-        credentialsFile = config.age.secrets.lego-token.path;
-        dnsProvider = "cloudflare";
-        email = "me@raroh73.xyz";
-      };
+      "shiori.raroh73.xyz" = { };
+      "webhook.raroh73.xyz" = { };
+    };
+    defaults = {
+      credentialsFile = config.age.secrets.lego-token.path;
+      dnsProvider = "cloudflare";
+      email = "me@raroh73.xyz";
     };
   };
 }
