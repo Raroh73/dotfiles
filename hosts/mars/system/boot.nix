80a9c3350308ccc271a7fe25b615fa5feb1fd20a
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-07-16 11:54:08 +0200

    feat: remove deprecated settings

--- hosts/mars/system/boot.nix
+++ hosts/mars/system/boot.nix
@@ -4,16 +4,9 @@
     kernelParams = [ "cma=256M" ];
     loader = {
       grub.enable = false;
-      raspberryPi = {
-        enable = true;
-        firmwareConfig = ''
-          gpu_mem=16
-        '';
-        uboot = {
+      generic-extlinux-compatible = {
           enable = true;
           configurationLimit = 8;
-        };
-        version = 3;
       };
     };
     tmp.cleanOnBoot = true;
