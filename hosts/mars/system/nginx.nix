92004f7dacc7edaecdff91830da89dec88a6460f
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-08-08 17:45:11 +0200

    feat: add authenticated-origin-pull

--- hosts/mars/system/nginx.nix
+++ hosts/mars/system/nginx.nix
@@ -1,8 +1,19 @@
-{ config, ... }: {
+{ config, ... }:
+let
+  cloudflare-origin-pull-ca = builtins.fetchurl {
+    url = "https://developers.cloudflare.com/ssl/static/authenticated_origin_pull_ca.pem";
+    sha256 = "0hxqszqfzsbmgksfm6k0gp0hsx9k1gqx24gakxqv0391wl6fsky1";
+  };
+in
+{
   services.nginx.virtualHosts = {
-    ${config.services.nextcloud.hostName} = {
+    "${config.services.nextcloud.hostName}" = {
       forceSSL = true;
       useACMEHost = "nextcloud.raroh73.xyz";
+      extraConfig = ''
+        ssl_client_certificate ${cloudflare-origin-pull-ca};
+        ssl_verify_client on;
+      '';
     };
   };
 }
