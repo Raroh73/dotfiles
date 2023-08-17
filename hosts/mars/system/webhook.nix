17d82ba81cca78b3e9fd7baffcfee2191a1e3ad0
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-08-17 12:03:31 +0200

    feat: use runtimeInputs

--- hosts/mars/system/webhook.nix
+++ hosts/mars/system/webhook.nix
@@ -2,17 +2,18 @@
 let
   raroh73_xyz-build = pkgs.writeShellApplication {
     name = "raroh73_xyz-build";
+    runtimeInputs = with pkgs; [ git hugo ];
     text = ''
       rm -fr /srv/web/raroh73.xyz/public
       if [ ! -d /srv/web/raroh73.xyz ]
       then
-        ${pkgs.git}/bin/git clone https://github.com/Raroh73/raroh73.xyz.git /srv/web/raroh73.xyz
+        git clone https://github.com/Raroh73/raroh73.xyz.git /srv/web/raroh73.xyz
         cd /srv/web/raroh73.xyz
       else
         cd /srv/web/raroh73.xyz
-        ${pkgs.git}/bin/git pull https://github.com/Raroh73/raroh73.xyz.git
+        git pull https://github.com/Raroh73/raroh73.xyz.git
       fi
-      ${pkgs.hugo}/bin/hugo --minify
+      hugo --minify
     '';
   };
 in
