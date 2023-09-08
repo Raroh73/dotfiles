d60ffbbd27b8ed35749aae1efc28f9c54bb69eef
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-09-08 14:55:32 +0200

    feat: add sol install-config

---
+++ hosts/sol/install-configuration.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }: {
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  boot.loader.systemd-boot = {
+    enable = true;
+    configurationLimit = 8;
+  };
+
+  networking = {
+    hostName = "sol";
+    networkmanager.enable = true;
+  };
+
+  services.openssh = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      KbdInteractiveAuthentication = false;
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
+
+  time.timeZone = "Europe/Amsterdam";
+
+  users.mutableUsers = false;
+
+  users.users.root.hashedPassword = "!";
+
+  users.users.raroh73 = {
+    isNormalUser = true;
+    extraGroups = [ "networkmanager" "wheel" ];
+    hashedPassword = "$y$j9T$hFvFXr7PoYmifluLzysxs0$hsK0SKi.cTyh7k8U8JqUX8c3lO9OfSw9mCXAHnSvB5/";
+    packages = with pkgs; [
+      git
+    ];
+  };
+
+  system.stateVersion = "22.11";
+}
