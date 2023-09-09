2e182fbdab228c4ee64985893940e6659bab197d
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-09-09 20:50:36 +0200

    feat: use scripts for sol installation

---
+++ hosts/sol/first-config.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Configure NixOS
+git clone https://github.com/Raroh73/dotfiles.git
+cd dotfiles
+sudo nixos-rebuild --flake . boot
+rm -fr dotfiles
+echo "First configuration finished!"
---
+++ hosts/sol/install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Create partitions
+parted /dev/vda -- mklabel gpt
+parted /dev/vda -- mkpart ESP fat32 1MiB 512MiB
+parted /dev/vda -- mkpart primary 512MiB -1GiB
+parted /dev/vda -- mkpart primary linux-swap -1GiB 100%
+parted /dev/vda -- set 1 esp on
+
+# Format partitions
+mkfs.fat -F32 -n boot /dev/vda1
+mkfs.ext4 -L nixos /dev/vda2
+mkswap -L swap /dev/vda3
+
+# Install NixOS
+mount /dev/disk/by-label/nixos /mnt
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/boot /mnt/boot
+swapon /dev/vda3
+nixos-generate-config --root /mnt
+curl https://raw.githubusercontent.com/Raroh73/dotfiles/main/hosts/sol/install-configuration.nix -o /mnt/etc/nixos/configuration.nix
+nixos-install --no-root-passwd
+echo "Installation finished!"
