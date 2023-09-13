d0d8b70cca3f034a2b29dc6ca383fbcd91998043
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-09-13 14:34:29 +0200

    feat: use bios

--- hosts/sol/install.sh
+++ hosts/sol/install.sh
@@ -1,27 +1,22 @@
 #!/usr/bin/env bash
 
 # Create partitions
-parted /dev/vda -- mklabel gpt
-parted /dev/vda -- mkpart ESP fat32 1MiB 512MiB
-parted /dev/vda -- mkpart primary 512MiB -1GiB
-parted /dev/vda -- mkpart primary linux-swap -1GiB 100%
+parted /dev/vda -- mklabel msdos
+parted /dev/vda -- mkpart primary 1MB -1GB
+parted /dev/vda -- mkpart primary linux-swap -1GB 100%
 parted /dev/vda -- set 1 esp on
 partprobe /dev/vda
 echo "Created partitions!"
 
 # Format partitions
-mkfs.fat -F32 -n boot /dev/vda1
-while [ ! -e "/dev/disk/by-label/boot" ]; do sleep 1; done
-mkfs.ext4 -L nixos /dev/vda2
+mkfs.ext4 -L nixos /dev/vda1
 while [ ! -e "/dev/disk/by-label/nixos" ]; do sleep 1; done
-mkswap -L swap /dev/vda3
+mkswap -L swap /dev/vda2
 while [ ! -e "/dev/disk/by-label/swap" ]; do sleep 1; done
 echo "Formatted partitions!"
 
 # Mount partitions
 mount /dev/disk/by-label/nixos /mnt
-mkdir -p /mnt/boot
-mount /dev/disk/by-label/boot /mnt/boot
 swapon /dev/disk/by-label/swap
 echo "Mounted partitions!"
 
