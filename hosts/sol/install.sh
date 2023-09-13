6900fc076455071c32d255959529174538fcb1b7
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-09-13 11:53:28 +0200

    fix: wait for partitions

--- hosts/sol/install.sh
+++ hosts/sol/install.sh
@@ -6,12 +6,16 @@ parted /dev/vda -- mkpart ESP fat32 1MiB 512MiB
 parted /dev/vda -- mkpart primary 512MiB -1GiB
 parted /dev/vda -- mkpart primary linux-swap -1GiB 100%
 parted /dev/vda -- set 1 esp on
+partprobe /dev/vda
 echo "Created partitions!"
 
 # Format partitions
 mkfs.fat -F32 -n boot /dev/vda1
+while [ ! -e "/dev/disk/by-label/boot" ]; do sleep 1; done
 mkfs.ext4 -L nixos /dev/vda2
+while [ ! -e "/dev/disk/by-label/nixos" ]; do sleep 1; done
 mkswap -L swap /dev/vda3
+while [ ! -e "/dev/disk/by-label/swap" ]; do sleep 1; done
 echo "Formatted partitions!"
 
 # Mount partitions
