0ab4a252f6448879e2a6a2a768f909d865f4ca9c
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-09-23 19:26:19 +0200

    style: sort mars home config

--- hosts/mars/home-configuration.nix
+++ hosts/mars/home-configuration.nix
@@ -21,16 +21,16 @@ _: {
       configFile.source =
         let
           default-config = builtins.fetchurl {
-            sha256 = "13xlp5sji9zfia0dpwlhirrjkfd498bpj9g3q1f1is9b0my0rj4d";
             url = "https://raw.githubusercontent.com/nushell/nushell/0.83.1/crates/nu-utils/src/sample_config/default_config.nu";
+            sha256 = "13xlp5sji9zfia0dpwlhirrjkfd498bpj9g3q1f1is9b0my0rj4d";
           };
         in
         "${default-config}";
       envFile.source =
         let
           default-env = builtins.fetchurl {
-            sha256 = "0qi1g1f32a5l0yp6nb6lizam6033masirfv0hjpd4a5rpp8c8dm1";
             url = "https://raw.githubusercontent.com/nushell/nushell/0.83.1/crates/nu-utils/src/sample_config/default_env.nu";
+            sha256 = "0qi1g1f32a5l0yp6nb6lizam6033masirfv0hjpd4a5rpp8c8dm1";
           };
         in
         "${default-env}";
