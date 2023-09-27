21c78896ac1b83e934e9e600b9b4d0e9e6c1cf88
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-09-27 21:15:15 +0200

    style: sort mars config

--- hosts/mars/configuration.nix
+++ hosts/mars/configuration.nix
@@ -334,9 +334,9 @@
       nginx.extraGroups = [ "acme" ];
       raroh73 = {
         extraGroups = [ "networkmanager" "wheel" ];
-        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+aFhxW7Q8vLMPCS8jPFtqUUePL6Ks9213gsEOJbIOz raroh73@mars" ];
         hashedPasswordFile = config.age.secrets.raroh73-mars-password.path;
         isNormalUser = true;
+        openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO+aFhxW7Q8vLMPCS8jPFtqUUePL6Ks9213gsEOJbIOz raroh73@mars" ];
         shell = pkgs.nushell;
       };
       root.hashedPassword = "!";
