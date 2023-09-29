daf17c88eb0d35d90863f01f1f88f04c48f72bcd
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-09-29 18:52:29 +0200

    refactor: rewrite earth install config

--- hosts/earth/install-configuration.nix
+++ hosts/earth/install-configuration.nix
@@ -3,13 +3,20 @@
     ./hardware-configuration.nix
   ];
 
-  nixpkgs.config.allowUnfree = true;
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader = {
+      efi.canTouchEfiVariables = true;
+      systemd-boot.enable = true;
+    };
+  };
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
   };
 
   networking = {
@@ -18,35 +25,56 @@
     networkmanager.enable = true;
   };
 
+  nix = {
+    nixPath = [ "nixpkgs=${pkgs.path}" ];
+    settings.experimental-features = [ "flakes" "nix-command" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  programs.git.enable = true;
+
+  services = {
+    fstrim.enable = true;
+    openssh = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        KbdInteractiveAuthentication = false;
+        PasswordAuthentication = false;
+        PermitRootLogin = "no";
+      };
+    };
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      jack.enable = true;
+      pulse.enable = true;
+    };
+    unbound.enable = true;
+    xserver = {
+      enable = true;
+      desktopManager.gnome.enable = true;
+      displayManager.gdm.enable = true;
+      videoDrivers = [ "nvidia" ];
+    };
+  };
+
+  system.stateVersion = "22.11";
+
   time.timeZone = "Europe/Warsaw";
-
-  services.xserver = {
-    enable = true;
-    desktopManager.gnome.enable = true;
-    displayManager.gdm.enable = true;
-    videoDrivers = [ "nvidia" ];
-
-  };
-
-  hardware.opengl = {
-    enable = true;
-    driSupport32Bit = true;
-  };
 
   users = {
     mutableUsers = false;
     users = {
       raroh73 = {
-        isNormalUser = true;
+        description = "Raroh73";
         extraGroups = [ "networkmanager" "wheel" ];
-        hashedPassword = "$6$x1KjgZ.8XadJt7ka$GI96zpDV6MeO9XYKxvC0VPb.oMh5wGrKSOxgStbSQ9Lsid.j2AUHmxn7JsKnN5puh251kgGIXmnxKgBDfuPMA1";
-        packages = with pkgs; [
-          git
-        ];
+        hashedPassword = "$y$j9T$G0rX4W6eqrwz2gNOSuM9/1$M1SiWyYp.Xq4u0GPAJxZYeW0CJ6q3BzIJ2ubkgi6DP3";
+        isNormalUser = true;
       };
       root.hashedPassword = "!";
     };
   };
-
-  system.stateVersion = "22.11";
 }
