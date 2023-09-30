77fb7a364f1b8990d4ba33ac8266edd67331de02
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-09-30 12:16:38 +0200

    style: group alsa settings

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -154,8 +154,10 @@
     };
     pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
       jack.enable = true;
       pulse.enable = true;
     };
--- hosts/earth/install-configuration.nix
+++ hosts/earth/install-configuration.nix
@@ -47,8 +47,10 @@
     };
     pipewire = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
       jack.enable = true;
       pulse.enable = true;
     };
