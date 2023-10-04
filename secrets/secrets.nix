7643280af08b8d4602b150cb174ef84db79ff40a
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2023-10-04 20:05:01 +0200

    chore: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   agenix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBerp1nA9AU2MUHk8/Fq+OvX0L2PiMqgQh12Zdm0cMyK agenix";
   earth = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOt72SaSNMlIcwwnmjAp3+oSAc9avEAkFyLdbS9xoShy root@earth";
-  mars = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFSXgqzlzIn12brvcJ9BXwcZmsYwDzjgNPH2zl74M8hT root@mars";
+  mars = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINlfc/SQR5rXj1MITk5C8Y5XyJ2ARUfvsoP+gVxZcs3E root@mars";
 in
 {
   "backups-nextcloud-environment.age".publicKeys = [ agenix mars ];
