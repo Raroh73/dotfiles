287e50d004a55c16b6839ba9bcdcae3e4df73262
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-01-16 14:34:31 +0100

    style: reformat git settings

--- hosts/earth/home-configuration.nix
+++ hosts/earth/home-configuration.nix
@@ -137,27 +137,13 @@
     git = {
       enable = true;
       extraConfig = {
-        apply = {
-          whitespace = "fix";
-        };
-        commit = {
-          gpgSign = true;
-        };
-        core = {
-          whitespace = "trailing-space,space-before-tab";
-        };
-        fetch = {
-          prune = true;
-        };
-        gpg = {
-          format = "ssh";
-        };
-        pull = {
-          ff = "only";
-        };
-        tag = {
-          gpgSign = true;
-        };
+        apply.whitespace = "fix";
+        commit.gpgSign = true;
+        core.whitespace = "trailing-space,space-before-tab";
+        fetch.prune = true;
+        gpg.format = "ssh";
+        pull.ff = "only";
+        tag.gpgSign = true;
         user = {
           #signingKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDtuv/+9ONhzcSoZRvlmyqtMNtQeFCQfEKfpA/4/dsY/ 96078496+Raroh73@users.noreply.github.com";
           signingKey = "~/.ssh/github.pub";
