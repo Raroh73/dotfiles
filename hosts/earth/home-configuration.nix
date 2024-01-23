bf278945f636d2bed6db7dd1346712794cb07b0c
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-01-23 20:20:35 +0100

    chore: update nushell config

--- hosts/earth/home-configuration.nix
+++ hosts/earth/home-configuration.nix
@@ -162,16 +162,16 @@
       configFile.source =
         let
           default-config = builtins.fetchurl {
-            url = "https://raw.githubusercontent.com/nushell/nushell/0.86.0/crates/nu-utils/src/sample_config/default_config.nu";
-            sha256 = "1pwxx79v5597cbf80kr8gfz1hkfnnffqq95vlvibsb7p309x468w";
+            url = "https://raw.githubusercontent.com/nushell/nushell/0.89.0/crates/nu-utils/src/sample_config/default_config.nu";
+            sha256 = "0qlhba79mryar4rh7k0s32vvhywqljy1v2z86grhp6p9c9m7ky83";
           };
         in
         "${default-config}";
       envFile.source =
         let
           default-env = builtins.fetchurl {
-            url = "https://raw.githubusercontent.com/nushell/nushell/0.86.0/crates/nu-utils/src/sample_config/default_env.nu";
-            sha256 = "122jb77r0kbgpzsigq04ysa9ydicxmpba0551ps7fwrzlyg9r0px";
+            url = "https://raw.githubusercontent.com/nushell/nushell/0.89.0/crates/nu-utils/src/sample_config/default_env.nu";
+            sha256 = "162nzdz7n9kjpl2k02dsv5vrawg7c3f7c4lp6822i93i9kl6k4vr";
           };
         in
         "${default-env}";
