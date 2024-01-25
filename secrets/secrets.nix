17f5ed7dc9bb3abcb982fb58d1aa50e18c48b3e2
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-01-25 13:16:43 +0100

    chore: rekey secrets

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -5,13 +5,13 @@ let
   sirius = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOYwq5b3AcyECOjwnI/C29AiIL3EEGCdg1y0tb6IN0oM root@sirius";
 in
 {
-  "backups-nextcloud-environment.age".publicKeys = [ agenix mars ];
-  "backups-nextcloud-password.age".publicKeys = [ agenix mars ];
-  "backups-nextcloud-repository.age".publicKeys = [ agenix mars ];
-  "cloudflare-dyndns-token.age".publicKeys = [ agenix mars ];
-  "lego-token.age".publicKeys = [ agenix mars ];
-  "nextcloud-adminpass.age".publicKeys = [ agenix mars ];
-  "nextcloud-secrets.age".publicKeys = [ agenix mars ];
+  "backups-nextcloud-environment.age".publicKeys = [ agenix mars sirius ];
+  "backups-nextcloud-password.age".publicKeys = [ agenix mars sirius ];
+  "backups-nextcloud-repository.age".publicKeys = [ agenix mars sirius ];
+  "cloudflare-dyndns-token.age".publicKeys = [ agenix mars sirius ];
+  "lego-token.age".publicKeys = [ agenix mars sirius ];
+  "nextcloud-adminpass.age".publicKeys = [ agenix mars sirius ];
+  "nextcloud-secrets.age".publicKeys = [ agenix mars sirius ];
   "raroh73-earth-password.age".publicKeys = [ agenix earth ];
   "raroh73-mars-password.age".publicKeys = [ agenix mars ];
   "raroh73-sirius-password.age".publicKeys = [ agenix sirius ];
