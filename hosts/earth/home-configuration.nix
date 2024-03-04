97bbfb9709e93cac94615b80832c3a90241852cb
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-03-04 14:32:55 +0100

    feat: change firefox settings

--- hosts/earth/home-configuration.nix
+++ hosts/earth/home-configuration.nix
@@ -104,34 +104,10 @@
       enable = true;
       profiles = {
         "Raroh73" = {
-          extensions = with pkgs.nur.repos.rycee.firefox-addons;
-            [ ublock-origin ];
-          search = {
-            default = "DuckDuckGo";
-            force = true;
-          };
-          settings = {
-            "app.update.auto" = false;
-            "browser.aboutConfig.showWarning" = false;
-            "browser.contentblocking.category" = "strict";
-            "browser.places.speculativeConnect.enabled" = false;
-            "browser.shell.checkDefaultBrowser" = false;
-            "browser.tabs.firefox-view" = false;
-            "browser.urlbar.speculativeConnect.enabled" = false;
-            "browser.urlbar.trimURLs" = false;
-            "dom.security.https_only_mode" = true;
-            "extensions.pocket.enabled" = false;
-            "network.dns.disablePrefetch" = true;
-            "network.http.speculative-parallel-limit" = 0;
-            "network.IDN_show_punycode" = true;
-            "network.predictor.enable-prefetch" = false;
-            "network.predictor.enabled" = false;
-            "network.prefetch-next" = false;
-            "network.trr.mode" = 5;
-            "signon.autofillForms" = false;
-            "signon.formlessCapture.enabled" = false;
-            "signon.rememberSignons" = false;
-          };
+          extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+            proton-pass
+            ublock-origin
+          ];
         };
       };
     };
