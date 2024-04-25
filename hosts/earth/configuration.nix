8ed31c5d5a8fea32cec45d200d7aa01a43deadcc
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-04-25 17:04:42 +0200

    feat: remove unused fonts

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -42,28 +42,6 @@
     };
   };
 
-  fonts = {
-    fontconfig = {
-      defaultFonts = {
-        emoji = [
-          "Noto Color Emoji"
-          "Noto Emoji"
-        ];
-        monospace = [ "Roboto Mono" ];
-        sansSerif = [ "Roboto" ];
-        serif = [ "Roboto Slab" ];
-      };
-    };
-    packages = with pkgs; [
-      (nerdfonts.override { fonts = [ "CascadiaCode" ]; })
-      noto-fonts
-      noto-fonts-emoji
-      roboto
-      roboto-mono
-      roboto-slab
-    ];
-  };
-
   hardware = {
     cpu.amd.updateMicrocode = true;
     enableRedistributableFirmware = true;
