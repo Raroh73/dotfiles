53de1bdbbcb09e8e77839b10d0547f151f0c7075
Raroh73 <96078496+Raroh73@users.noreply.github.com>
2024-04-25 16:41:04 +0200

    style: reorder stylix options

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -216,11 +216,11 @@
   };
 
   stylix = {
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tomorrow-night.yaml";
     image = pkgs.fetchurl {
     url = "https://w.wallhaven.cc/full/43/wallhaven-43lej9.png";
     hash = "sha256-QY9EnxPoHXlo6J0q1sDdkdC6Jl69RMPnlvOOT3ljqqk=";
   };
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tomorrow-night.yaml";
   };
 
   swapDevices = [ { device = "/dev/disk/by-uuid/e369f49e-b657-4e07-89d8-acdbe6b12b25"; } ];
